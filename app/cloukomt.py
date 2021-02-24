113f4d41affc1eddf49db21ce1229ab6d302399c
asverty <asverty@gmail.com>
2021-02-24 16:10:17 +0300

    Begin the project

---
+++ app/cloukomt.py
@@ -0,0 +1,8 @@
+from cloudpayments import CloudPayments
+import config
+
+public_id = config.PUBLIC_ID
+api_secret = config.API_SECRET
+
+client = CloudPayments('public_id', 'api_secret')
+test(self, request_id=None)
